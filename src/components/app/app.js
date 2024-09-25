686a8a3aced79507c0f645fdb49987104980ac95
nooraiym <140681537+nooraiym@users.noreply.github.com>
2024-09-25 20:07:20 +0500

    feat: add filter through user input

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -17,6 +17,7 @@ class App extends Component {
         { name: 'Flex C.', salary: 1400, increase: true, rise: false, id: 2 },
         { name: 'Samuel C.', salary: 5000, increase: false, rise: false, id: 3 },
       ],
+      term: ''
     }
     this.maxId = 4
   }
@@ -46,9 +47,20 @@ class App extends Component {
     this.setState(({ data }) => ({ data: data.map(item => (item.id === id ? { ...item, [prop]: !item[prop] } : item)) }))
   }
 
+  searchEmployee = (items, term) => {
+    if(term.length === 0 ) return items
+    return items.filter(item => item.name.indexOf(term) > -1)
+  }
+
+  onUpdateSearch = (term) => {
+    this.setState({term})
+  }
+
   render() {
+    const {data, term} = this.state
     const employeesCount = this.state.data.length
     const bonusedEmployeesCount = this.state.data.filter(employee => employee.increase).length
+    const visibleData = this.searchEmployee(data, term)
     return (
       <div className='app'>
         <AppInfo
@@ -57,12 +69,14 @@ class App extends Component {
         />
 
         <div className='search-panel'>
-          <SearchPanel />
+          <SearchPanel
+            onUpdateSearch={this.onUpdateSearch}
+          />
           <AppFilter />
         </div>
 
         <EmployeesList
-          data={this.state.data}
+          data={visibleData}
           onDelete={this.deleteItem}
           onToggleProp={this.onToggleProp}
         />
