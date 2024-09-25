a0adbc7faaaa855b1c7ef23982aa63797560d761
nooraiym <140681537+nooraiym@users.noreply.github.com>
2024-09-25 15:12:25 +0500

    refactor data and items

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -13,48 +13,58 @@ class App extends Component {
     super(props)
     this.state = {
       data: [
-        {name: 'Alex C.', salary: 4000, increase: false, id: 1},
-        {name: 'Flex C.', salary: 1400, increase: true, id: 2},
-        {name: 'Samuel C.', salary: 5000, increase: false, id: 3},
-      ]
+        { name: 'Alex C.', salary: 4000, increase: false, rise: false, id: 1 },
+        { name: 'Flex C.', salary: 1400, increase: true, rise: false, id: 2 },
+        { name: 'Samuel C.', salary: 5000, increase: false, rise: false, id: 3 },
+      ],
     }
     this.maxId = 4
   }
 
-  deleteItem = (id) => {
-    this.setState(({data}) => {
+  deleteItem = id => {
+    this.setState(({ data }) => {
       return {
-        data: data.filter(item => item.id !== id)
+        data: data.filter(item => item.id !== id),
       }
     })
   }
 
   addItem = (name, salary) => {
-    const newEmployee = {name, salary, increase: false, id: this.maxId++}
-    this.setState(({data}) => {
+    const newEmployee = { name, salary, increase: false, rise: false, id: this.maxId++ }
+    this.setState(({ data }) => {
       return {
         data: [...data, newEmployee],
       }
     })
   }
 
+  onToggleProp = (id, prop) => {
+    this.setState(({ data }) => ({ data: data.map(item => (item.id === id ? { ...item, [prop]: !item[prop] } : item)) }))
+  }
+
   render() {
-      return (
-        <div className='app'>
-          <AppInfo />
+    const employeesCount = this.state.data.length
+    const bonusedEmployeesCount = this.state.data.filter(employee => employee.increase).length
+    return (
+      <div className='app'>
+        <AppInfo
+          employeesCount={employeesCount}
+          bonusedEmployeesCount={bonusedEmployeesCount}
+        />
 
-          <div className='search-panel'>
-            <SearchPanel />
-            <AppFilter />
-          </div>
-
-          <EmployeesList
-            data={this.state.data}
-            onDelete={this.deleteItem}
-          />
-          <EmployeesAddForm onAdd={this.addItem} />
+        <div className='search-panel'>
+          <SearchPanel />
+          <AppFilter />
         </div>
-      )
+
+        <EmployeesList
+          data={this.state.data}
+          onDelete={this.deleteItem}
+          onToggleProp={this.onToggleProp}
+        />
+        <EmployeesAddForm onAdd={this.addItem} />
+      </div>
+    )
   }
 }
 
--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -1,28 +1,23 @@
-import { Component } from "react";
-
 import EmployeesListItem from "../employees-list-item/employees-list-item";
 
 import './employees-list.css';
 
-class EmployeesList extends Component {
-    constructor(props) {
-        super(props)
-    }
-
-    render() {
-        const { data, onDelete } = this.props
-        const elements = data.map(employee => {
-                    const { id, ...employeeProps } = employee
-                    return (
-                        <EmployeesListItem
-                            key={id}
-                            {...employeeProps}
-                            onDelete={() => onDelete(id)}
-                        />
-                    )
-                })
-        return <ul className='app-list list-group'>{elements}</ul>
-    }
+const EmployeesList = (props) => {
+    const { data, onDelete, onToggleProp } = props
+    const elements = data.map(employee => {
+                const { id, ...employeeProps } = employee
+                return (
+                    <EmployeesListItem
+                        key={id}
+                        {...employeeProps}
+                        onDelete={() => onDelete(id)}
+                        onToggleProp={(e) => {
+                            onToggleProp(id, e.currentTarget.getAttribute('data-toggle'))
+                        }}
+                    />
+                )
+            })
+    return <ul className='app-list list-group'>{elements}</ul>
 }
 
 export default EmployeesList;
